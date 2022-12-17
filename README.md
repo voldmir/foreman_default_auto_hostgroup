40922f6cf12bd3c3bad60e15fe376743a9f2dc02
Vladimir Savchenko <voldmir@mail.ru>
2022-12-17 20:53:22 +0700

    release 1.0.0

--- README.md
+++ README.md
@@ -4,11 +4,14 @@ Plugin to auto add hostgroup to host.
 
 ## Installation
 
+msgfmt -o ./LC_MESSAGES/foreman_default_auto_hostgroup.mo ./foreman_default_auto_hostgroup.po
+
 See Foreman's [plugin installation documentation](https://theforeman.org/plugins/#2.Installation).
 
 ## Compatibility
 
 | Foreman Version | Plugin Version |
 | --------------- | -------------: |
+| ~> 1.24         |          0.1.2 |
 | ~> 1.24         |          1.0.0 |
 
--- app/models/setting/default_auto_hostgroup.rb
+++ app/models/setting/default_auto_hostgroup.rb
@@ -1,5 +1,5 @@
 class Setting::DefaultAutoHostgroup < ::Setting
-  BLANK_ATTRS.concat(%w{hosts_to_exclude default_name_hostgroup parameter_name_hostgroup fact_name_match})
+  BLANK_ATTRS.concat(%w{hosts_to_exclude default_name_hostgroup fact_name_match})
 
   def self.load_defaults
     return unless ActiveRecord::Base.connection.table_exists?("settings")
@@ -7,11 +7,8 @@ class Setting::DefaultAutoHostgroup < ::Setting
 
     Setting.transaction do
       [
-        self.set("force_hostgroup_match", _("Apply hostgroup matching even if a host already has one."), false),
-        self.set("force_hostgroup_match_only_new", _("Apply hostgroup matching only on new hosts"), true),
-        self.set("force_host_environment", _("Apply hostgroup's environment to host even if a host already has a different one"), true),
+        self.set("enable_auto_hostgroup", _("Enable host group mapping."), false),
         self.set("fact_name_match", _("Fact name to match"), ""),
-        self.set("parameter_name_hostgroup", _("Parameter name hostgroup"), ""),
         self.set("default_name_hostgroup", _("Default host group"), ""),
         self.set("hosts_to_exclude", _("hosts to exclude"), []),
       ].compact.each { |s| create(s.update(category: "Setting::DefaultAutoHostgroup")) }
