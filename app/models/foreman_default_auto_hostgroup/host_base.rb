dd773212341c7ac8282080880d9770e3c9c33233
Vladimir Savchenko <voldmir@mail.ru>
2023-05-17 21:23:57 +0700

    refactoring

--- app/models/foreman_default_auto_hostgroup/host_base.rb
+++ app/models/foreman_default_auto_hostgroup/host_base.rb
@@ -8,20 +8,28 @@ module ForemanDefaultAutoHostgroup
 
         return result unless Setting[:enable_auto_hostgroup]
         return result unless Setting[:fact_name_match]
-        #return result unless facts_hash[Setting[:fact_name_match]].present?
         return result if Setting[:hosts_to_exclude].map { |r| Regexp.new(r).match?(hostname) }.include?(true)
 
-        new_hostgroup = find_hostgroup()
+        def_hostgroup = find_hostgroup_default()
+        new_hostgroup = find_hostgroup() || def_hostgroup
 
         return result unless new_hostgroup
-        return result if self.hostgroup == new_hostgroup
 
-        self.hostgroup = new_hostgroup
-        self.environment = new_hostgroup.environment
+        organization = new_hostgroup.organizations.first
 
-        save(validate: false)
-        Rails.logger.info "DefaultAutoHostgroupMatch: #{hostname} added to #{new_hostgroup}"
+        if new_hostgroup.organizations.size != 1 or not organization.present?
+          new_hostgroup = def_hostgroup
+          organization = find_organization_default()
+        end
 
+        self.hostgroup = new_hostgroup unless self.hostgroup == new_hostgroup
+        self.environment = new_hostgroup.environment unless self.environment == new_hostgroup.environment
+        self.organization = organization unless self.organization == organization
+
+        if self.changed.present?
+          save(validate: false)
+          Rails.logger.info "DefaultAutoHostgroupMatch: #{hostname} added to #{new_hostgroup}"
+        end
         result
       end
     end
@@ -30,16 +38,20 @@ module ForemanDefaultAutoHostgroup
       prepend Overrides
     end
 
+    def find_organization_default()
+      Taxonomy.unscoped.find_by(name: Setting[:default_organization])
+    end
+
+    def find_hostgroup_default()
+      Hostgroup.unscoped.find_by(name: Setting[:default_name_hostgroup])
+    end
+
     def find_hostgroup()
       if facts_hash[Setting[:fact_name_match]].present?
         hg = Hostgroup.unscoped.find_by(title: facts_hash[Setting[:fact_name_match]])
         return hg if hg.present?
       end
-
-      hg = Hostgroup.unscoped.find_by(name: Setting[:default_name_hostgroup])
-      return hg if hg.present?
-
-      Rails.logger.info "No match hostgroup..."
+      Rails.logger.info "No hostgroup match with fact_name_match..."
       false
     end
   end
